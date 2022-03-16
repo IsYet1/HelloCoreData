30b5ad1f1e4924a8a828499a0f30309de997f4bc
DonMck <donmck@amazon.com>
2022-03-16 14:06:34 -0500

    Initial Commit

---
+++ HelloCoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloCoreData
+//
+//  Created by Don McKenzie on 16-Mar-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
